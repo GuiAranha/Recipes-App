e7f70cc0e47c5dfc18584c70d79a4237c2492887
Guilherme Lara <guilaraa@gmail.com>
2022-04-27 18:51:09 -0300

    Requisitos 19 ao 24

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Drinks() {
   return (
-    <Header pageName="Drinks" needRender type="drink" />
+    <>
+      <Header pageName="Drinks" needRender type="drink" />
+      <span>Estou em bebidas</span>
+      <Footer />
+    </>
   );
 }
 
--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Foods() {
   return (
     <>
       <Header pageName="Foods" needRender type="food" />
       <div>Estou no foods</div>
+      <Footer />
     </>
   );
 }
