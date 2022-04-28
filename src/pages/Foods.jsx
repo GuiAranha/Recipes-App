2b9e81324ffd23ef11c124a83e65940544cba9f5
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-04-28 09:11:45 -0300

    commit para fazer o pull

--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -4,11 +4,7 @@ import Header from '../components/Header';
 function Foods() {
   return (
     <>
-<<<<<<< HEAD
       <Header pageName="Foods" needRender />
-=======
-      <Header pageName="Foods" needRender type="food" />
->>>>>>> 5bcf25c4697bdb16d8b60ecadc68552034b365ef
       <div>Estou no foods</div>
     </>
   );
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
 import Header from '../components/Header';
 
-<<<<<<< HEAD
 export default function profile() {
-=======
-function Profile() {
->>>>>>> 5bcf25c4697bdb16d8b60ecadc68552034b365ef
   return (
     <Header pageName="Profile" needRender={ false } />
   );
 }
-<<<<<<< HEAD
-=======
-
-export default Profile;
->>>>>>> 5bcf25c4697bdb16d8b60ecadc68552034b365ef
