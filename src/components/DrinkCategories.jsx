e37c7cda0bddba82fa133ea396bb73c60c76142e
Bruno Bastos <brunobastos@Mac-mini-de-Bruno.local>
2022-05-02 17:02:50 -0300

    Requisito 28 - Adicionar botoes de categoria para filtrar - APROVADO

--- src/components/DrinkCategories.jsx
+++ src/components/DrinkCategories.jsx
@@ -3,11 +3,18 @@ import { Button } from 'react-bootstrap';
 import MyContext from '../context/MyContext';
 
 function DrinkCategories() {
-  const { drinkCategories } = useContext(MyContext);
+  const { drinkCategories, setAllDrink } = useContext(MyContext);
   const MAX_RENDER = 5;
   if (drinkCategories.length > MAX_RENDER) {
     drinkCategories.length = 5;
   }
+
+  const applyCategoryFilter = async (category) => {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`;
+    const data = await fetch(url);
+    const { drinks } = await data.json();
+    setAllDrink(drinks);
+  };
 
   return (
     <div>
@@ -17,6 +24,7 @@ function DrinkCategories() {
           <Button
             data-testid={ `${strCategory}-category-filter` }
             type="button"
+            onClick={ () => applyCategoryFilter(strCategory) }
           >
             {strCategory}
 
--- src/components/FoodCategories.jsx
+++ src/components/FoodCategories.jsx
@@ -3,11 +3,18 @@ import { Button } from 'react-bootstrap';
 import MyContext from '../context/MyContext';
 
 function FoodCategories() {
-  const { foodCategories } = useContext(MyContext);
+  const { foodCategories, setAllFood } = useContext(MyContext);
   const MAX_RENDER = 5;
   if (foodCategories.length > MAX_RENDER) {
     foodCategories.length = 5;
   }
+
+  const applyCategoryFilter = async (category) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
+    const data = await fetch(url);
+    const { meals } = await data.json();
+    setAllFood(meals);
+  };
 
   return (
     <div>
@@ -17,6 +24,7 @@ function FoodCategories() {
           <Button
             data-testid={ `${strCategory}-category-filter` }
             type="button"
+            onClick={ () => applyCategoryFilter(strCategory) }
           >
             {strCategory}
 
