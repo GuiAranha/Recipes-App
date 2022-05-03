877644833bd0fc49f2b8887ee42a2665ecbed336
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-05-03 13:54:30 -0300

    main-group-26 requisito 67-68-69

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -6,6 +6,7 @@ function ExploreDrinks() {
   return (
     <div>
       <Header pageName="Explore Drinks" needRender={ false } />
+      <div> Estou na Explore Drinks</div>
       <Footer />
     </div>
   );
